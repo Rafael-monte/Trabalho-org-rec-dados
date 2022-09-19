5bc3406427e2a338646b359e862e09e10c2b092e
Rafael-monte <rafaelmonte46@gmail.com>
2022-09-19 12:43:44 -0300

    Busca implementada, iniciando insercao

--- files/file-handler/file-handler.c
+++ files/file-handler/file-handler.c
@@ -41,46 +41,23 @@ void ImportarRegistroEmArquivo(struct Processo* processo)
 
 FILE* CopiarConteudoDoArquivo(FILE* arquivoQueSeraCopiado, FILE* arquivoFinal)
 {
-    char campo[TAM_STR];
-    char buffer[TAM_MAX_REG];
-    char charAtual;
-    int posicaoCaractereAtual=0;
-    int posicaoAtual = 0;
-    LimparBuffer(buffer);
-    while((charAtual = fgetc(arquivoQueSeraCopiado)) != EOF) 
-    {
-        if (charAtual != '\n')
-        {
-           buffer[posicaoCaractereAtual] = charAtual;
-           buffer[posicaoCaractereAtual+1]='\0';
-           posicaoCaractereAtual++;
+    short comp_reg;
+
+    char linha[512];
+    while (fgets(linha, 512, arquivoQueSeraCopiado) != NULL) {
+        if(linha[strlen(linha) - 1] == '\n'){
+            linha[strlen(linha) - 1] = '\0';
         }
-        else
-        {
-            char registroFinal[2056];
-            short tamanho_registro = strlen(buffer);
-            char tamEmString[9];
-            itoa(tamanho_registro, tamEmString, 10);
-            strcpy(registroFinal, tamEmString);
-            // strcat(registroFinal, "|");
-            strcat(registroFinal, buffer);
-            fputs(registroFinal, arquivoFinal);
-            posicaoAtual++;
-            posicaoCaractereAtual = 0;
-            LimparBuffer(buffer);
-        }
+        comp_reg = strlen(linha);
+        char tamanho_em_string[10];
+        char registro_final[512];
+        strcpy(registro_final, itoa(comp_reg, tamanho_em_string, 10));
+        strcat(registro_final, DELIM_STR);
+        strcat(registro_final, linha);
+        fputs(registro_final, arquivoFinal);
     }
-    char registroFinal[2056];
-    short tamanho_registro = strlen(buffer);
-    char tamEmString[9];
-    itoa(tamanho_registro, tamEmString, 10);
-    strcpy(registroFinal, tamEmString);
-    // strcat(registroFinal, "|");
-    strcat(registroFinal, buffer);
-    fputs(registroFinal, arquivoFinal);
-    posicaoAtual++;
-    posicaoCaractereAtual = 0;
-    LimparBuffer(buffer);
+
+    return arquivoFinal;
 }
 
 
@@ -97,7 +74,7 @@ void OperacoesEmLote(struct Processo* processo)
 {
     printf("Arquivo -> %s \n", processo->nome_arq_principal);
     printf("Arquivo secundario ->%s\n", processo->nome_arq_busca);
-    FILE* arquivoOrigem = AbrirArquivo(processo->nome_arq_principal, "r+");
+    FILE* arquivoOrigem = AbrirArquivo(processo->nome_arq_principal, "rb");
     FILE* arquivo_comandos = AbrirArquivo(processo->nome_arq_busca, "r+");
     printf("Iniciando operacoes em lote!\n\n");
     struct Processo* comandos = ProcessarArquivoComandos(arquivo_comandos);
@@ -110,8 +87,11 @@ void OperacoesEmLote(struct Processo* processo)
         {
             BuscarRegistro(arquivoOrigem, pivot->parametro_operacao);
         }
-        
-        
+
+        if (pivot->operacao == 'i')
+        {
+            InserirRegistro(arquivoOrigem, pivot->parametro_operacao);
+        }
         pivot = pivot->proximo_processo;
     }
 
@@ -119,35 +99,75 @@ void OperacoesEmLote(struct Processo* processo)
 }
 
 
-void BuscarRegistro(FILE* arquivo_dados, char* id_reg)
+int BuscarRegistro(FILE* arquivo_dados, char* id_reg)
 {
-    printf("Id buscado:%s\n", id_reg);
-
-    char buffer[500];
-    char* id_atual;
-    int achou = 0;
-    short comp_reg;
-    comp_reg = leia_reg(buffer, 500, arquivo_dados);
-
-
-    while(!achou && comp_reg > 0)
-    {
-        id_atual = strtok(buffer, "|");
-        printf("Id atual:%s\n",id_atual);
-        if (strcmp(id_atual, id_reg) == 0)
-        {
-            achou = 1;
-        } 
-        else 
-        {
-            comp_reg = leia_reg(buffer, 500, arquivo_dados);
+    int offset = 0;
+    char linha[512];
+    char prev_line[512];
+    
+    char tam_reg[4];
+    buscar_campo(tam_reg, 4, arquivo_dados);
+    while(!feof(arquivo_dados)){
+        offset = ftell(arquivo_dados);
+        char reg[atoi(tam_reg)];
+        fgets(reg, atoi(tam_reg), arquivo_dados);
+        char * codigo_registro = buscar_codigo_registro(reg);
+        if(atoi(codigo_registro) == atoi(id_reg)){
+            printf("Registro encontrado: %s\n", reg);
+            return offset;
         }
+        buscar_campo(tam_reg, 4, arquivo_dados);
+        
     }
+    printf("Registro nao encontrado \n");
+    return -1;
+}
 
 
-    if (achou)
+char* buscar_codigo_registro(char registro[512]){
+    char auxiliar[512];
+    strcpy(auxiliar, registro);
+    char * codigo_registro = strtok(auxiliar, DELIM_STR);
+    return codigo_registro;
+}
+
+
+int buscar_campo(char* campo, int tam, FILE* arquivo_dados){
+    int i = 0;
+    char ch = fgetc(arquivo_dados);
+    while (ch != EOF && ch != '|')
     {
-        printf("Id encontrado!\n");
+        if (i < tam-1) {
+            campo[i] = ch;
+            i++;
+        }
+        ch = fgetc(arquivo_dados);
+    }
+    campo[i] = '\0';
+    return i;
+}
+
+
+
+int InserirRegistro(FILE* arquivo_dados, char registro[256])
+{
+    printf("Iniciando insercao do registro %s\n", registro);
+
+    char *token = buscar_codigo_registro(registro);
+    int offset = BuscarRegistro(arquivo_dados, token);
+    if(offset == -1){
+        printf("Adicionando registro no final do arquivo...\n");
+        int comp_reg = strlen(registro);
+        char tamanho_em_string[10];
+        char registro_final[512];
+        strcpy(registro_final, itoa(comp_reg, tamanho_em_string, 10));
+        strcat(registro_final, DELIM_STR);
+        strcat(registro_final, registro);
+        fputs(registro_final, arquivo_dados);
+    }else{
+        printf("Codigo ja existe \n");
+        printf("RRN: %i \n", offset);
+        fseek(arquivo_dados, offset, SEEK_SET);
     }
 }
 
--- files/file-handler/file-handler.h
+++ files/file-handler/file-handler.h
@@ -32,9 +32,13 @@ void AdicionarCampoAoArquivo(char* campo, FILE* arquivoDestino);
 void LimparBuffer(char buffer[]);
 
 
-void BuscarRegistro(FILE* arquivo_dados, char* id_reg);
+int BuscarRegistro(FILE* arquivo_dados, char* id_reg);
 
+int InserirRegistro(FILE* arquivo_dados, char registro[256]);
 
-short leia_reg(char * buffer, int tam, FILE* arq);
+int buscar_campo(char* campo, int tam, FILE* arquivo_dados);
+
+char* buscar_codigo_registro(char registro[512]);
+
 
 #endif //TRABALHO_ORG_E_REC_DADOS_FILE_HANDLER_H
